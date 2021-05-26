9e8758c53ee2acd362d0e3738b14ce05176f23ca
Nneka Ufumaka <ufumakanneka@gmail.com>
2021-05-26 12:41:19 -0400

    commit tree 5049cc09696fc74c5a55722495f11fb06ac8e4aa

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,19 @@
-export function manageFriends(state, action){
+export function manageFriends(state= {friends: [] }, action){
+
+    switch (action.type) {
+        case 'ADD_FRIEND':
+          return {friends: [...state.friends,  action.friend]}
+        case 'REMOVE_FRIEND':
+            const removalIndex = state.friends.findIndex(friend => friend.id === action.id)
+            return(
+                {friends:[
+                ...state.friends.slice(0,removalIndex),
+                ...state.friends.slice(removalIndex + 1)
+                ]}
+            )
+        default:
+          return state;
+      }
+    
+  
 }
